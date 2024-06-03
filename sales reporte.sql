4b71cdb03d53c65804dee743bc1ec0ddd7434f82
Trishna Chakraborty <158463954+Myselftrishnaofficial@users.noreply.github.com>
2024-06-03 17:21:31 +0530

    Add files via upload

---
+++ sales reporte.sql
@@ -0,0 +1,23 @@
+create table sales(
+ProductCategory varchar2(50),
+ProductName varchar2(30),
+SaleAmount number);
+--insert
+INSERT into sales values('electronics','laptop',1000);
+INSERT into sales values('electronics','Phone',800);
+INSERT into sales values('electronics','tablet',500);
+INSERT into sales values('clothing','shirt',300);
+INSERT into sales values('clothing','pants',400);
+INSERT into sales values('furniture','sofa',1200);
+INSERT into sales values('furniture','bed',900);
+--select
+select ProductCategory,ProductName,SaleAmount from sales;
+
+SELECT 
+    ProductCategory,
+    SUM(SaleAmount) AS TotalSales
+FROM 
+    sales
+GROUP BY 
+    ROLLUP(ProductCategory);
+
